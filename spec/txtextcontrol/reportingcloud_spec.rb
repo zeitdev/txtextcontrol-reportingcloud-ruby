3034032f166f5a4f36dc4ed4100e150f58b38cb3
Thorsten Kummerow <thorsten@textcontrol.com>
2016-05-30 17:39:21 +0200

    Adding one text

--- spec/txtextcontrol/reportingcloud_spec.rb
+++ spec/txtextcontrol/reportingcloud_spec.rb
@@ -11,10 +11,21 @@ describe "#listTemplates" do
     @r = TXTextControl::ReportingCloud::ReportingCloud.new("username", "password")
   end
   
-  it "should return three template file names" do
+  it "returns three template file names" do
     canned_response = File.new File.dirname(__FILE__) + '/../support/fixtures/listTemplates.json'
     stub_request(:get, "api.reporting.cloud/v1/templates/list").to_return(:body => canned_response)
     fileNames = @r.listTemplates
     expect(fileNames.length).to be(3)
+  end
+end
+
+describe "#getTemplateCount" do
+  before do
+    @r = TXTextControl::ReportingCloud::ReportingCloud.new("username", "password")
+  end
+  
+  it "returns five" do
+    stub_request(:get, "api.reporting.cloud/v1/templates/count").to_return(:body => "5")
+    expect(@r.getTemplateCount).to be(5)
   end
 end
