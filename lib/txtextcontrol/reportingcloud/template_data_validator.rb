5fec96d84ebd2a7c037e1601437c31ca047be08a
Thorsten Kummerow <thorsten@textcontrol.com>
2016-06-02 18:01:58 +0200

    Adding documentation.

--- lib/txtextcontrol/reportingcloud/template_data_validator.rb
+++ lib/txtextcontrol/reportingcloud/template_data_validator.rb
@@ -4,6 +4,10 @@ module TXTextControl
     # Internal method parameter validator class.
     # @author Thorsten Kummerow
     class TemplateDataValidator
+    
+      # Checks if a given value is a String, is not nil and not empty.
+      # @param template_data [String] The string to validate.
+      # @return [Boolean]
       def self.validate(template_data)
         raise ArgumentError, "Template data must be a Base64 encoded string." if !template_data.kind_of? String
         raise ArgumentError, "No template data given." if template_data.to_s.empty?
--- lib/txtextcontrol/reportingcloud/template_name_validator.rb
+++ lib/txtextcontrol/reportingcloud/template_name_validator.rb
@@ -4,9 +4,13 @@ module TXTextControl
     # Internal method parameter validator class.
     # @author Thorsten Kummerow
     class TemplateNameValidator
-      def self.validate(templateName)
-        raise ArgumentError, "Template name must be a String." if !templateName.kind_of? String 
-        raise ArgumentError, "No template name given." if templateName.to_s.empty?
+    
+      # Checks if a given value is a String, is not nil and not empty.
+      # @param template_name [String] The string to validate.
+      # @return [Boolean]
+      def self.validate(template_name)
+        raise ArgumentError, "Template name must be a String." if !template_name.kind_of? String 
+        raise ArgumentError, "No template name given." if template_name.to_s.empty?
       end
     end
   end
